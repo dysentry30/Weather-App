245543c6b729f350e9114299d2a2dec1c59e3832
dysentry30 <bagassatrianurwinanto@gmail.com>
2021-11-27 00:06:09 +0700

    Adding cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const server = express();
 const PORT = process.env.PORT || 8080;
 const rootPath = path.join(__dirname, "/public");
 
 server.use(express.json());
+server.use(cors);
 server.use(express.static(`${__dirname}/public`));
 server.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 
