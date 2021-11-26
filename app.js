4bbe92d53ec46ad2239d3a07a993b43391acf127
dysentry30 <bagassatrianurwinanto@gmail.com>
2021-11-27 00:07:54 +0700

    Configure cors

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const path = require("path");
 const server = express();
 const PORT = process.env.PORT || 8080;
 const rootPath = path.join(__dirname, "/public");
-
+var corsOptions = {
+    origin: "https://weather-app-simple-clone.herokuapp.com/",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 server.use(express.json());
 server.use(cors);
 server.use(express.static(`${__dirname}/public`));
